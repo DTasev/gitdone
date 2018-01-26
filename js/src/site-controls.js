ef1df4e27221123f0c5c939ea25bc356f4051882
Dimitar Tasev <dimtasev@gmail.com>
2018-01-26 17:18:03 +0000

    Added button for additional repository options

--- js/src/site-controls.js
+++ js/src/site-controls.js
@@ -8,3 +8,12 @@ function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
 }
+
+function showRepositoryOptions() {
+    var x = document.getElementById("repository-options");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
