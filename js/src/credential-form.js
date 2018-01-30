e91c9e2f65a32d63bcdbbc7a7bb7a7a633b9cca6
Diomtgrfba <dbt@aber.ac.uk>
2018-01-30 12:51:32 +0000

    Now hides credential after a successful request, closes #48

---
+++ js/src/credential-form.js
@@ -0,0 +1,10 @@
+function CredentialForm () {}
+
+CredentialForm.show = function () {
+    document.getElementById("user-credential-form").hidden = false;
+}
+
+
+CredentialForm.hide = function () {
+    document.getElementById("user-credential-form").hidden = true;
+}
