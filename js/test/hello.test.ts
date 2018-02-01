cbfa75f27f928a67aa805e5b0717a207826bfacb
Dimitar Tasev <dimtasev@gmail.com>
2018-02-01 19:25:39 +0000

    Added changes for test running

---
+++ js/test/hello.test.ts
@@ -0,0 +1,11 @@
+import { hello } from '../ts/hello';
+
+import { expect } from 'chai';
+import 'mocha';
+
+describe("Hello function", () => {
+    it("should return hello world", () => {
+        const result = hello();
+        expect(result).to.be.equal("Hello world!");
+    });
+});
